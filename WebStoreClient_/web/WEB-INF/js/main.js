3d2ea5933b7293af35c13a8600cbfb88dab745f2
Viacheslav <slavik.golovn.99@gmail.com>
2017-12-12 15:54:41 +0200

    Внесение правок

---
+++ WebStoreClient_/web/WEB-INF/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {
+    
+    $("#products-types li").click(function (){
+        check(this, "#products-types li");
+    });
+    
+    $("#left-menu-nav > li").click(function (){
+        if($(this).text() === $("#products-types").text()) {return;}
+    });
+    
+    $("#manufs > *").click(function (){
+        check(this, "#manufs > *");
+    });
+    
+        
+});
+
+function show(el) {
+    if ($(el).hasClass("disabled")) {
+        $(el).removeClass("disabled");
+    } else {
+        $(el).addClass("disabled");
+    }
+}
+
+function check(el, mass) {
+    if (!$(el).hasClass("checked")) {
+        clearChecked(mass);
+        $(el).addClass("checked");
+    } else {
+        clearChecked(mass);
+    }
+}
+
+function clearChecked(el) {
+    $(el).map(function (){
+        $(this).removeClass("checked");
+    });
+}
---
+++ WebStoreClient_/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 12.12.2017, 15:23:47
+    Author     : Вячеслав
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JSP Page</title>
+        <%@include file="WEB-INF/jspf/head.jspf" %>
+    </head>
+    <body>
+        <%@include file="WEB-INF/jspf/leftmenu.jspf" %>
+    </body>
+</html>
